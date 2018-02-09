80d562b4cac5b0494f10ac67746a6be17441115d
jskny <jskny.goo@gmail.com>
2018-02-09 14:51:33 +0900

    add search-form css style.

--- searchform.php
+++ searchform.php
@@ -1,6 +1,5 @@
 <!-- searchform.php -->
-<form method="get" id="searchform" action="<?php echo home_url( '/' ); ?>" >
-	<input type="text" placeholder="検索" name="s" id="s" value="" />
-	<input type="submit" id="searchsubmit" value="検索" />
+<form method="get" id="search-form" action="<?php echo home_url( '/' ); ?>" >
+	<input type="text" placeholder="検索" name="s" id="search-text" value="" /><input type="submit" id="search-submit" value="検索" />
 </form>
 <!-- /searchform.php -->
